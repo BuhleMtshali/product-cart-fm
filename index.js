b6de52dd66a870577f860f278bf6333c367dc44f
Buhle <mtshalizanobuhle7@gmail.com>
2024-12-14 23:41:13 +0200

    updated the javscaript file

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+import data from './data.json';
+
 //defining variables
 let menuItemsContainer = document.getElementById('menu-container');
 let cartCount = document.getElementById('cart-count');
@@ -6,5 +8,6 @@ let placeholderElement = document.getElementById('placeholder');
 
 //function for rendering elements
 function renderMenu() {
-    
+
 }
+console.log(data)

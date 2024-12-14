974bc4fe1eec59524418f37dc740f6980ba54dbc
Buhle <mtshalizanobuhle7@gmail.com>
2024-12-14 23:19:24 +0200

    updated the javascript file

---
+++ index.js
@@ -0,0 +1,10 @@
+//defining variables
+let menuItemsContainer = document.getElementById('menu-container');
+let cartCount = document.getElementById('cart-count');
+let placeholderElement = document.getElementById('placeholder');
+
+
+//function for rendering elements
+function renderMenu() {
+    
+}
